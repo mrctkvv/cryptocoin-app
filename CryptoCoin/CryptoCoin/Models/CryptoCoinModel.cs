3f2115dd6c9478c7f6ec2873233bc785af65fee4
Mariia Loboda <mariia.loboda.wrk@gmail.com>
2023-10-23 21:46:34 +0300

    Added cryptocoin model

---
+++ CryptoCoin/CryptoCoin/Models/CryptoCoinModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoCoin.Models
+{
+    public class CryptoCoinModel
+    {
+        public string? Id { get; set; }
+        public string? Rank { get; set; }
+        public string? Symbol { get; set; }
+        public string? Name { get; set; }
+        public string? Supply { get; set; }
+        public string? MaxSupply { get; set; }
+        public string? MarketCapUsd { get; set; }
+        public string? VolumeUsd24Hr { get; set; }
+        public string? PriceUsd { get; set; }
+        public string? ChangePercent24Hr { get; set; }
+        public string? VWap24Hr { get; set; }
+        public string? Explorer { get; set; }
+    }
+}
